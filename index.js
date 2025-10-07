e8b0abdfafd95eff5ce25f6eff9c2aeba31b0e40
Omar Dukureh <132103818+oduku1@users.noreply.github.com>
2025-10-07 12:12:40 -0400

    new

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import express from "express";
+
+const app = express();
+
+
+
+app.set("view engine", "ejs"); // tells Express to use EJS
+app.set("views", "./views");  
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
--- routes/addapplication.js
+++ routes/addapplication.js
@@ -8,12 +8,4 @@ const router = express.Router()
 
 
 
-
-
-
-
-
-
-
-
 export default router
